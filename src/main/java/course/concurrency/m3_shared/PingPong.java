f59baf176b33ab6c382c7b5556f497933492e787
Nikolai Kostromitin <emailbin@yandex.ru>
2023-11-05 15:00:37 +0500

    Add Ping Pong using Object.wait/notify

--- src/main/java/course/concurrency/m3_shared/PingPong.java
+++ src/main/java/course/concurrency/m3_shared/PingPong.java
@@ -1,18 +1,37 @@
 package course.concurrency.m3_shared;
 
 public class PingPong {
+    private static final Object lock = new Object();
 
     public static void ping() {
-
+        synchronized (lock) {
+            while (!Thread.currentThread().isInterrupted()) {
+                System.out.println("Ping");
+                lock.notify();
+                try {
+                    lock.wait();
+                } catch (InterruptedException ignored) {
+                }
+            }
+        }
     }
 
     public static void pong() {
-
+        synchronized (lock) {
+            while (!Thread.currentThread().isInterrupted()) {
+                System.out.println("Pong");
+                lock.notify();
+                try {
+                    lock.wait();
+                } catch (InterruptedException ignored) {
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
-        Thread t1 = new Thread(() -> ping());
-        Thread t2 = new Thread(() -> pong());
+        Thread t1 = new Thread(PingPong::ping);
+        Thread t2 = new Thread(PingPong::pong);
         t1.start();
         t2.start();
     }
