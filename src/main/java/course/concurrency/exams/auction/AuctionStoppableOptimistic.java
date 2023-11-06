5a36541ca8a1cb9c05593fb2443d6724ff437100
Nikolai Kostromitin <emailbin@yandex.ru>
2023-11-06 13:04:17 +0500

    Add AuctionStoppable with pessimistic and optimistic blocking

--- src/main/java/course/concurrency/exams/auction/AuctionStoppableOptimistic.java
+++ src/main/java/course/concurrency/exams/auction/AuctionStoppableOptimistic.java
@@ -1,29 +1,53 @@
 package course.concurrency.exams.auction;
 
+import java.util.concurrent.atomic.AtomicMarkableReference;
+
 public class AuctionStoppableOptimistic implements AuctionStoppable {
 
     private Notifier notifier;
+
+    private final AtomicMarkableReference<Bid> latestBid = new AtomicMarkableReference<>(null, false);
 
     public AuctionStoppableOptimistic(Notifier notifier) {
         this.notifier = notifier;
     }
 
-    private Bid latestBid;
-
     public boolean propose(Bid bid) {
-        if (bid.getPrice() > latestBid.getPrice()) {
-            notifier.sendOutdatedMessage(latestBid);
-            latestBid = bid;
-            return true;
+        if (latestBid.isMarked()) {
+            return false;
+        }
+
+        if (latestBid.getReference() == null) {
+            if (latestBid.compareAndSet(null, bid, false, false)) {
+                return true;
+            }
+        }
+
+        Bid current = latestBid.getReference();
+        if (!latestBid.isMarked() && bid.getPrice() > current.getPrice()) {
+            do {
+                if (latestBid.compareAndSet(current, bid, false, false)) {
+                    notifier.sendOutdatedMessage(current);
+                    return true;
+                }
+                current = latestBid.getReference();
+            } while (!latestBid.isMarked() && bid.getPrice() > current.getPrice());
         }
         return false;
     }
 
     public Bid getLatestBid() {
-        return latestBid;
+        return latestBid.getReference();
     }
 
     public Bid stopAuction() {
-        return latestBid;
+        Bid current = latestBid.getReference();
+        do {
+            if (latestBid.attemptMark(current, true)) {
+                break;
+            }
+            current = latestBid.getReference();
+        } while (!latestBid.isMarked());
+        return current;
     }
 }
--- src/main/java/course/concurrency/exams/auction/AuctionStoppablePessimistic.java
+++ src/main/java/course/concurrency/exams/auction/AuctionStoppablePessimistic.java
@@ -1,29 +1,68 @@
 package course.concurrency.exams.auction;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 public class AuctionStoppablePessimistic implements AuctionStoppable {
 
     private Notifier notifier;
+
+    private volatile Bid latestBid;
+
+    private final ReentrantReadWriteLock proposeLock = new ReentrantReadWriteLock();
+    private volatile boolean isStopped;
 
     public AuctionStoppablePessimistic(Notifier notifier) {
         this.notifier = notifier;
     }
 
-    private Bid latestBid;
-
     public boolean propose(Bid bid) {
-        if (bid.getPrice() > latestBid.getPrice()) {
-            notifier.sendOutdatedMessage(latestBid);
-            latestBid = bid;
+        if (isStopped) {
+            return false;
+        }
+
+        if (!isStopped && latestBid == null) {
+            proposeLock.writeLock().lock();
+            if (!isStopped && latestBid == null) {
+                latestBid = bid;
+            }
+            proposeLock.writeLock().unlock();
             return true;
         }
+
+        if (!isStopped && bid.getPrice() > latestBid.getPrice()) {
+            proposeLock.writeLock().lock();
+            try {
+                if (!isStopped && bid.getPrice() > latestBid.getPrice()) {
+                    latestBid = bid;
+                    notifier.sendOutdatedMessage(latestBid);
+                    return true;
+                }
+            } finally {
+                proposeLock.writeLock().unlock();
+            }
+        }
+
         return false;
     }
 
     public Bid getLatestBid() {
-        return latestBid;
+        proposeLock.readLock().lock();
+        try {
+            return latestBid;
+        } finally {
+            proposeLock.readLock().unlock();
+        }
     }
 
     public Bid stopAuction() {
-        return latestBid;
+        proposeLock.readLock().lock();
+        try {
+            isStopped = true;
+            return latestBid;
+        } finally {
+            proposeLock.readLock().unlock();
+        }
     }
 }
